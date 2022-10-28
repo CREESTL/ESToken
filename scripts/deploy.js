945aa46e6071bc0f1b1dc7fb6fe85e0f1bca210d
degorov <degorov@sfxdx.com>
2022-10-28 13:24:48 +0300

    Redeploy to Mainnet and Fix Address.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -19,8 +19,8 @@ if (network.name == "chapel") {
   // BUSDImplementation
   USDT_ADDRESS = "0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee";
 } else if (network.name == "bsc") {
-  // Binance-Peg BUSD
-  USDT_ADDRESS = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+  // BEP20USDT
+  USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955";
 }
 
 async function main() {
